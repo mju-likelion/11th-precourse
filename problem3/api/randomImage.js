f5dd7bf8ed1180eebe6d0024e1789ea20d5d4f27
Bhanjo <hanjo1515@naver.com>
2023-04-02 23:51:01 +0900

    feat: problem3 carousel 코드 추가

--- problem3/api/randomImage.js
+++ problem3/api/randomImage.js
@@ -1,1 +1,17 @@
-// 여기에 데이터 패치를 합니다
+/** 이미지 리스트를 받습니다. */
+const getRandomImage = async () => {
+  const randomAlbumId = Math.floor(Math.random() * (10 - 1) + 1);
+  // 보통, api 주소는 노출시키지 않는 것이 원칙입니다! (.gitignore에 대해 조사해보세요)
+  const BASE_URL = `https://jsonplaceholder.typicode.com/albums/${randomAlbumId}/photos`;
+
+  try {
+    const result = await fetch(BASE_URL);
+    const content = await result.json();
+
+    return content;
+  } catch (e) {
+    console.log('api 에러!', e.message);
+  }
+};
+
+export default getRandomImage;
--- problem3/index.js
+++ problem3/index.js
@@ -1,1 +1,89 @@
-// 여기에 Carousel을 조작할 코드를 작성합니다.
+import getRandomImage from './api/randomImage.js';
+
+const randomImageBtn = document.querySelector('#getImageButton');
+const container = document.querySelector('#imageContainer');
+const counter = document.querySelector('#imageCounter');
+const leftBtn = document.querySelector('#moveLeft');
+const rightBtn = document.querySelector('#moveRight');
+
+window.addEventListener('DOMContentLoaded', () => {
+  registButtonEvent();
+});
+
+let totalCount = 0;
+let currentCount = 0;
+
+const registButtonEvent = () => {
+  randomImageBtn.addEventListener('click', async () => {
+    const imageData = await getRandomImage();
+    const slicedImageData = imageData.slice(0, 10); // 실습상 50개는 너무 많아 10개로 줄입니다
+
+    container.innerHTML = '';
+    totalCount = 0;
+    currentCount = 0;
+    container.scrollTo({
+      left: 0,
+      behavior: 'smooth',
+    });
+
+    slicedImageData.forEach((data) => renderImage(data));
+    totalCount = slicedImageData.length;
+    renderCounter(0);
+  });
+
+  leftBtn.addEventListener('click', () => moveImage('left'));
+  rightBtn.addEventListener('click', () => moveImage('right'));
+};
+
+/** @param {{url: string, id: string, title: string}} data */
+const renderImage = (data) => {
+  const divEl = document.createElement('div');
+  const imgEl = document.createElement('img');
+  const pEl = document.createElement('p');
+
+  imgEl.src = data.url;
+  imgEl.alt = '슬라이드이미지';
+
+  pEl.textContent = data.title;
+
+  divEl.append(imgEl);
+  divEl.append(pEl);
+  container.append(divEl);
+};
+
+/** @param {number} currentIndex */
+const renderCounter = (currentCount) => {
+  counter.textContent = `${currentCount + 1} / ${totalCount}`;
+};
+
+/**
+ * Carousel을 움직입니다.
+ * @param {'left' | 'right'} type
+ */
+const moveImage = (type) => {
+  if (type === 'left') moveLeft();
+  if (type === 'right') moveRight();
+};
+
+const moveLeft = () => {
+  if (currentCount > 0) {
+    container.scrollTo({ left: --currentCount * 600, behavior: 'smooth' });
+  } else {
+    container.scrollTo({ left: totalCount * 600, behavior: 'smooth' });
+    currentCount = totalCount - 1;
+  }
+  renderCounter(currentCount);
+};
+
+const moveRight = () => {
+  if (currentCount < totalCount - 1) {
+    container.scrollTo({
+      left: ++currentCount * 600,
+      behavior: 'smooth',
+    });
+  } else {
+    container.scrollTo({ left: 0, behavior: 'smooth' });
+    currentCount = 0;
+  }
+  renderCounter(currentCount);
+};
