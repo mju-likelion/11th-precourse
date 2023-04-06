4293e29e6db9d8a2b27f69f4375e4374380816ae
Bhanjo <hanjo1515@naver.com>
2023-04-07 00:47:56 +0900

    feat: 삭제, 업데이트 로직 구현

--- session2/js/index.js
+++ session2/js/index.js
@@ -6,32 +6,11 @@
 //     user: string타입,
 //   },
 // ];
-// test todo
-// const todos = [
-//   {
-//     id: 0,
-//     complete: true,
-//     content: '테스트입니당',
-//     user: 'test',
-//   },
-//   {
-//     id: 1,
-//     complete: false,
-//     content: '하이하이',
-//     user: 'test',
-//   },
-//   {
-//     id: 2,
-//     complete: false,
-//     content: '다른유저',
-//     user: 'hanjo',
-//   },
-// ];
-// localStorage.setItem('todos', JSON.stringify(todos));
 
 const todoContainerEl = document.querySelector('#todoContainer');
 const todoInputEl = document.querySelector('#todoInput');
 const todoButtonEl = document.querySelector('#todoButton');
+const logoutButtonEl = document.querySelector('#logoutButton');
 
 /** 로그인 되었는지 판별합니다. */
 const isLogin = () => {
@@ -48,21 +27,32 @@ const readTodo = () => {
 
   const todos = JSON.parse(localStorage.getItem('todos'));
 
+  // todo를 보여줄 DOM을 그립니다.
   todos.forEach((todo) => {
     const divEl = document.createElement('div');
     const idEl = document.createElement('p');
     const completeEl = document.createElement('input');
     const contentEl = document.createElement('p');
     const userEl = document.createElement('p');
+    const deleteEl = document.createElement('button');
 
     completeEl.type = 'checkbox';
+    completeEl.addEventListener('click', () =>
+      updateComplete(todo.id, completeEl.checked)
+    );
+
+    if (todo.complete) contentEl.style.textDecoration = 'line-through';
+
+    deleteEl.type = 'button';
+    deleteEl.textContent = '삭제';
+    deleteEl.addEventListener('click', () => deleteTodo(todo.id));
 
     idEl.textContent = todo.id;
     completeEl.checked = todo.complete;
     contentEl.textContent = todo.content;
     userEl.textContent = todo.user;
 
-    divEl.append(idEl, completeEl, contentEl, userEl);
+    divEl.append(idEl, completeEl, contentEl, userEl, deleteEl);
     todoContainerEl.append(divEl);
   });
 };
@@ -71,7 +61,7 @@ const createTodo = () => {
   const todoText = todoInputEl.value;
 
   const todos = JSON.parse(localStorage.getItem('todos'));
-  const newId = todos[todos.length - 1].id + 1;
+  const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
 
   const newTodo = {
     id: newId,
@@ -89,11 +79,46 @@ const createTodo = () => {
   readTodo();
 };
 
+const deleteTodo = (deleteId) => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  const filterdTodos = todos.filter((todo) => todo.id !== deleteId);
+
+  // 삭제하고 나머지 todos를 다시 로컬 스토리지에 저장합니다. 그후 다시 todo를 그립니다.
+  localStorage.setItem('todos', JSON.stringify(filterdTodos));
+  readTodo();
+};
+
+const updateComplete = (updateId, isComplete) => {
+  // 체크 변경할 때마다 필터링해서 저장하고 다시 그립니다.
+  const todos = JSON.parse(localStorage.getItem('todos'));
+
+  // 업데이트할 todo를 찾아서 해당 todo의 complete를 바꾼다. 그게 아니라면 그대로 전달합니다.
+  const changedTodos = todos.map((todo) =>
+    todo.id === updateId ? { ...todo, complete: isComplete } : todo
+  );
+
+  localStorage.setItem('todos', JSON.stringify(changedTodos));
+  readTodo();
+};
+
+const logout = () => {
+  alert('로그아웃!');
+  localStorage.removeItem('login');
+  location.href = './signin.html';
+};
+
 const init = () => {
   isLogin();
+
+  // 로컬스토리지에 todos가 없다면 빈 배열을 추가해줍니다.
+  if (!localStorage.getItem('todos')) {
+    localStorage.setItem('todos', JSON.stringify([]));
+  }
+
   readTodo();
 
   todoButtonEl.addEventListener('click', createTodo);
+  logoutButtonEl.addEventListener('click', logout);
 };
 
 document.addEventListener('DOMContentLoaded', init);
