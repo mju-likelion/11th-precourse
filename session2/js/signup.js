2a918d6e60354952ded581c3edb32d48eae134a2
Bhanjo <hanjo1515@naver.com>
2023-04-07 00:48:22 +0900

    fix: 유저가 없을 때 todo 예외 처리

--- session2/js/signup.js
+++ session2/js/signup.js
@@ -6,6 +6,10 @@ const passwordEl = document.querySelector('#signupPassword');
 const isUserExist = (newUserId) => {
   // 여기선 우리가 입력한 정보를 '객체'로 받아서 로직을 짜보겠습니다.
   const users = localStorage.getItem('userList');
+
+  // 등록된 유저가 없다면 바로 리턴
+  if (!users) return false;
+
   const convertedUsers = JSON.parse(users);
   const getExistUser = convertedUsers.find((user) => user.id === newUserId);
 
@@ -15,12 +19,24 @@ const isUserExist = (newUserId) => {
 /** @param {{id: string, password: string}} userInfo */
 const registerUser = (userInfo) => {
   const currentUsers = JSON.parse(localStorage.getItem('userList'));
-  const updatedUsers = currentUsers.concat({
-    id: userInfo.id,
-    password: userInfo.password,
-  });
 
-  localStorage.setItem('userList', JSON.stringify(updatedUsers));
+  // 등록된 유자가 없다면 새로운 배열을 만들어서 넣기
+  if (!currentUsers) {
+    const newUserList = [];
+    newUserList.push({
+      id: userInfo.id,
+      password: userInfo.password,
+    });
+
+    localStorage.setItem('userList', JSON.stringify(newUserList));
+  } else {
+    const updatedUsers = currentUsers.concat({
+      id: userInfo.id,
+      password: userInfo.password,
+    });
+
+    localStorage.setItem('userList', JSON.stringify(updatedUsers));
+  }
 };
 
 const init = () => {
