7e11e16109cf243e788b31886fa73b33d403348a
Bhanjo <hanjo1515@naver.com>
2023-05-06 22:40:21 +0900

    feat: Object 문제1 추가

---
+++ js-beginner/step2/problem/Object/p1.js
@@ -0,0 +1,30 @@
+// person Object를 만드세요
+// 객체의 key, value에 대해 간단히 검색해 학습하세요.
+// person은 first = 'kim', lastname = 'namu', age = 20, from = 'korea', job = 'programmer'을 가집니다.
+
+// 위에서 만든 person 객체에서 lastname을 'gildong'으로 바꾸세요.
+
+// 위에서 만든 person 객체에서 score 프로퍼티를 추가하고 해당 프로퍼티의 값은 100으로 설정하세요.
+
+// 위에서 만든 person 객체에서 from 프로퍼티를 삭제하세요.
+
+// person2 객체를 만들고 person 객체를 복사하세요.
+
+// person2 객체의 job을 'student'로 바꾸고 lastname을 'babo'로 바꾸세요.
+
+// person과 person2의 job을 비교하세요. 이때, person의 job은 'programmer'이고 person2의 job은 'student'가 되어야힙니다.
+// 마찬가지고 person의 lastname은 namu이고 person2의 lastname은 'babo'가 되어야합니다.
+
+// person의 요소를 하나씩 출력하세요
+// for..in을 이용해 출력하세요
+
+// person이 가지고 있는 key를 하나씩 출력하세요 (마지막 문제 연관)
+
+// person이 가지고 있는 value를 하나씩 출력하세요
+
+// person의 score에 50을 빼고, person2의 score에 50을 더하세요
+
+// person의 key, value를 참고하여
+// personArray 변수에 ['fist=kim', 'lastname=gildong', 'age=20', 'from=korea', 'job=programmer', 'score=50']과 같은 배열로 만드세요.
+// 단, personArray.push(`first=${person.first}`) personArray.push(`first=${person.first}`)와 같은 하드코딩은 하지 마세요.
+// 한트: '마지막 문제 연관' 이라고 쓰여진 문제와 Array 다루는 법을 조합하세요.
